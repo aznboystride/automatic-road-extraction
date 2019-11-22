3bb1a558121209f882e0280aa88ba27832b50a51
aznboystride <FairNuri.aboshehwa@student.csulb.edu>
2019-11-21 17:34:28 -0800

    modified train.py to save acc and loss on all sets

--- train.py
+++ train.py
@@ -10,101 +10,7 @@ import torch.optim as optim
 import torch.utils.data as data
 from datetime import datetime
 from pytz import timezone
-
-parser = argparse.ArgumentParser(description='trainer')
-
-parser.add_argument('-lr',  '--learning_rate',  type=float, required=True,  dest='lr',          help='learning rate')
-parser.add_argument('-b',   '--batch',          type=int,   required=True,  dest='batch',       help='batch size')
-parser.add_argument('-it',  '--iterations',     type=int,   required=True,  dest='iterations',  help='# of iterations')
-parser.add_argument('-dv',  '--devices',        type=str,   required=True,  dest='devices',     help='gpu indices sep. by comma')
-parser.add_argument('-wt',  '--weights',        type=str,   required=True,  dest='weights',     help='path to weights file')
-parser.add_argument('-lw',  '--lweights',       type=str,   required=False, dest='lweights',    help='name of weights file to load')
-parser.add_argument('-au'   '--augment',        type=str,   required=False, dest='augment',     help='name of augmentation')
-parser.add_argument('-s'    '--stats',          type=int,   required=False, dest='stats',       help='print statistics')
-parser.add_argument('-ls',  '--loss',           type=str,   required=False, dest='loss',        help='name of loss')
-parser.add_argument('-e',   '--epoch',          type=int,   required=False, dest='epoch',       help='epoch start')
-parser.add_argument('model', type=str, help='name of model')
-
-# SMOOTH = 1e-6
-
-# def iou_pytorch(outputs, labels):
-#     outputs[outputs>=0.5] = 1
-#     outputs[outputs<0.5] = 0
-#     outputs = outputs.int()
-#     labels = labels.int()
-#     # You can comment out this line if you are passing tensors of equal shape
-#     # But if you are passing output from UNet or something it will most probably
-#     # be with the BATCH x 1 x H x W shape
-#     outputs = outputs.squeeze(1)  # BATCH x 1 x H x W => BATCH x H x W
-#
-#     intersection = (outputs & labels).float().sum((1, 2))  # Will be zero if Truth=0 or Prediction=0
-#     union = (outputs | labels).float().sum((1, 2))         # Will be zzero if both are 0
-#
-#     iou = (intersection + SMOOTH) / (union + SMOOTH)  # We smooth our devision to avoid 0/0
-#
-#     #thresholded = torch.clamp(20 * (iou - 0.5), 0, 10).ceil() / 10  # This is equal to comparing with thresolds
-#
-#     return iou.mean()  # Or thresholded.mean() if you are interested in average across the batch
-
-minValLoss = sys.maxsize
-
-tries = 0
-def validate(model, trainloader):
-    model.eval()
-    global criterion
-    global minValLoss
-    global no_optim
-    global tries
-    running_loss = 0
-    counter = batch_multiplier
-    batchloss = 0
-    batchcount = 0
-    for i, (inputs, labels) in enumerate(trainloader, 1):
-        if (len(trainloader) - i + 1) < args.batch:
-            break
-        inputs = inputs.cuda()
-        labels = labels.cuda()
-        if counter == 0:
-            counter = batch_multiplier
-            running_loss += batchloss
-            batchcount += 1
-            batchloss = 0
-
-        outputs = model(inputs)
-        loss = criterion(outputs, labels) / batch_multiplier
-        batchloss += loss.item()
-        counter -= 1
-
-    if running_loss / batchcount < minValLoss:
-        print('[+] validation -- new better loss  %.5f -> %.5f ' % (minValLoss, running_loss / batchcount))
-        old_path = 'weights/{}_{}_{:.5f}_val.pth'.format(args.weights, criterion.__class__.__name__, minValLoss)
-        os.system('rm ' + old_path + ' ' + old_path.replace('weights', 'optimizers'))
-        minValLoss = running_loss / batchcount
-        savepath = 'weights/{}_{}_{:.5f}_val.pth'.format(args.weights, criterion.__class__.__name__, minValLoss)
-        torch.save(model.state_dict(), savepath)
-        torch.save(optimizer.state_dict(), savepath.replace("weights", "optimizers"))
-        no_optim = 0
-        tries = 0
-    elif no_optim >= 5:
-        print("[-] validation -- loss %.5f" % (running_loss / batchcount))
-        loadpath = 'weights/{}_{}_{:.5f}_val.pth'.format(args.weights, criterion.__class__.__name__, minValLoss)
-        model.load_state_dict(torch.load(loadpath))
-        lr = args.lr / 5
-        for param_group in optimizer.param_groups:
-            param_group['lr'] = lr
-        print("[!] New learning rate %.5f -> %.5f" % (args.lr, lr))
-        args.lr = lr
-        no_optim = 0
-        tries += 1
-    elif tries == 7:
-        print("[-] validation -- loss %.5f" % (running_loss / batchcount))
-        print("[!] Early stop")
-    else:
-        print("[-] validation -- loss %.5f" % (running_loss / batchcount))
-        no_optim += 1
-    print()
-
-    model.train()
+from sklearn.metrics import jaccard_similarity_score as jsc
 
 
 class ValidDataset(data.Dataset):
@@ -140,18 +46,90 @@ class Dataset(data.Dataset):
         return len(self.loader)
 
 
-args = parser.parse_args()
+parser = argparse.ArgumentParser(description='trainer')
 
-# def update_lr(optimizer,net):
-#     with open('learning_rate', 'r') as f:
-#         lr = float(f.read())
-#     if lr == args.lr:
-#         return
-#     print("New learning rate {} -> {}".format(args.lr, lr))
-#     args.lr = lr
-#     net.load_state_dict(torch.load("weights/{}.pth".format(args.weights)))
-#     for param_group in optimizer.param_groups:
-#         param_group['lr'] = lr
+parser.add_argument('-lr',  '--learning_rate',  type=float, required=True,  dest='lr',          help='learning rate')
+parser.add_argument('-b',   '--batch',          type=int,   required=True,  dest='batch',       help='batch size')
+parser.add_argument('-it',  '--iterations',     type=int,   required=True,  dest='iterations',  help='# of iterations')
+parser.add_argument('-dv',  '--devices',        type=str,   required=True,  dest='devices',     help='gpu indices sep. by comma')
+parser.add_argument('-wt',  '--weights',        type=str,   required=True,  dest='weights',     help='path to weights file')
+parser.add_argument('-lw',  '--lweights',       type=str,   required=False, dest='lweights',    help='name of weights file to load')
+parser.add_argument('-au'   '--augment',        type=str,   required=False, dest='augment',     help='name of augmentation')
+parser.add_argument('-s'    '--stats',          type=int,   required=False, dest='stats',       help='print statistics')
+parser.add_argument('-ls',  '--loss',           type=str,   required=False, dest='loss',        help='name of loss')
+parser.add_argument('-e',   '--epoch',          type=int,   required=False, dest='epoch',       help='epoch start')
+parser.add_argument('model', type=str, help='name of model')
+
+
+'''
+What should I save for validation? Best loss and Accuracy.
+Should I save the optimizer? No. Just save best training loss optimizer.
+What should I name the validation weights? val_loss_<modelname>_<criterion>_<loss>.pth
+Name validation and accuracy base on criterion used / model and quantity.
+Loss should be bce + ssim.
+'''
+
+minValLoss = float('inf')
+maxValAcc = 0.0
+def iou(outputs, labels):
+    labels = labels.cpu().numpy().reshape(-1)
+    outputs = outputs.cpu().numpy().reshape(-1)
+    return jsc(outputs, labels)
+
+def validate():
+    global minValLoss
+    global maxValAcc
+    model.eval()
+    running_loss = 0
+    running_acc = 0
+    counter = batch_multiplier
+    batchloss = 0
+    batchacc  = 0
+    batchcount = 0
+    for i, (inputs, labels) in enumerate(validloader, 1):
+        if len(validloader) + 1 - i < args.batch:
+            break
+        inputs = inputs.cuda()
+        labels = labels.cuda()
+        if counter == 0:
+            counter = batch_multiplier
+            running_loss += batchloss
+            running_acc += batchacc
+            batchcount += 1
+            batchloss = 0
+            batchacc = 0
+        
+        outputs = model(inputs)
+        loss = criterion(outputs, labels) / batch_multiplier
+        acc = iou(outputs, labels) / batch_multiplier
+        batchloss += loss.item()
+        batchacc += acc
+        counter -= 1
+
+    if running_loss / batchcount < minValLoss:
+        print('[+] validation -- new better loss  %.5f -> %.5f\n' % (minValLoss, running_loss / batchcount))
+        old_path = 'weights/val_loss_{}_{}_{:.5f}.pth'.format(args.model, criterion.__class__.__name__, minValLoss)
+        if os.path.exists(old_path):
+            os.system('rm ' + old_path)
+        minValLoss = running_loss / batchcount
+        savepath = 'weights/val_loss_{}_{}_{:.5f}.pth'.format(args.model, criterion.__class__.__name__, minValLoss)
+        torch.save(model.state_dict(), savepath)
+    else:
+        print("[-] validation -- loss %.5f\n" % (running_loss / batchcount))
+
+
+    if running_acc / batchcount > maxValAcc:
+        print('[+] validation -- new better acc  %.5f -> %.5f\n' % (maxValAcc, running_acc / batchcount))
+        old_path = 'weights/val_acc_{}_{}_{:.5f}.pth'.format(args.model, "iouscore", maxValAcc)
+        if os.path.exists(old_path):
+            os.system('rm ' + old_path)
+        maxValAcc = running_acc / batchcount
+        savepath = 'weights/val_acc_{}_{}_{:.5f}.pth'.format(args.model, "iouscore", maxValAcc)
+        torch.save(model.state_dict(), savepath)
+    else:
+        print("[-] validation -- acc %.5f\n" % (running_acc / batchcount))
+
+args = parser.parse_args()
 
 args.stats = 30 if not args.stats else args.stats
 
@@ -184,39 +162,35 @@ model.cuda()
 
 optimizer = None
 if args.lweights:
-    model.load_state_dict(torch.load("weights/{}.pth".format(args.lweights)))
-    optimizer = optim.Adam(model.parameters())
-    optimizer.load_state_dict(torch.load("optimizers/{}.pth".format(args.lweights)))
+    model.load_state_dict(torch.load("weights/{}".format(args.lweights)))
+    optimizer = optim.RMSProp(model.parameters(), lr=args.lr, weight_decay=1e-4)
+    optimizer.load_state_dict(torch.load("optimizers/{}".format(args.lweights)))
 
 dataset = Dataset(test=False, augment=augment)
 
 trainloader = torch.utils.data.DataLoader(
     dataset,
     batch_size=len(ids) * 4,
-    drop_last=True,
     shuffle=True)
 
 validloader = torch.utils.data.DataLoader(ValidDataset(), batch_size=len(ids) * 4, shuffle=True)
 
 criterion = nn.BCELoss() if not criterion else criterion
 
-optimizer = optim.Adam(model.parameters()) if not optimizer else optimizer
-
 print('Training start')
 print('Arguments -> {}'.format(' '.join(sys.argv)))
-best_loss = len(trainloader) * 100
 batch_multiplier = args.batch / (len(ids) * 4)
-# with open('learning_rate', 'w+') as f:
-#     f.write(str(args.lr))
 
-no_optim = 0
-best_train_loss = len(trainloader) * 100
+minTrainLoss = float('inf')
+maxTrainAcc = 0.0
 for epoch in range(args.epoch, args.iterations + args.epoch):
     print("[+] Epoch ({}/{}) - {}".format(epoch, args.iterations + args.epoch,
                                           datetime.now(timezone("US/Pacific")).strftime("%m-%d-%Y - %I:%M %p")))
     running_loss = 0
+    running_acc = 0
     counter = batch_multiplier
     batchloss = 0
+    batchacc = 0
     batchcount = 0
     for i, (inputs, labels) in enumerate(trainloader, 1):
         if (len(trainloader) - i + 1) < args.batch:
@@ -228,29 +202,46 @@ for epoch in range(args.epoch, args.iterations + args.epoch):
             optimizer.zero_grad()
             counter = batch_multiplier
             running_loss += batchloss
+            running_acc += batchacc
             batchcount += 1
-            # if batchcount % args.stats == 0:
-            #     print('training -- [%d, %5d] %s loss: %.5f time: %s' %
-            #             (epoch, batchcount, criterion.__class__.__name__, running_loss/batchcount, datetime.now(timezone("US/Pacific")).strftime("%m-%d-%Y - %I:%M %p")))
             batchloss = 0
+            batchacc = 0
         counter -= 1
 
         outputs = model(inputs)
         loss = criterion(outputs, labels) / batch_multiplier
         loss.backward()
+        with torch.no_grad():
+            acc = iou(outputs, labels)
         batchloss += loss.item()
+        batchacc += acc
 
-    if running_loss / batchcount < best_train_loss / batchcount:
-        print('[+] training -- new better loss  %.5f -> %.5f ' % (
-        best_train_loss / batchcount, running_loss / batchcount))
-        best_train_loss = running_loss
-        # torch.save(model.state_dict(), "weights/" + args.weights + ".pth")
-        # torch.save(optimizer.state_dict(), "optimizers/" + args.weights + ".pth")
+    if running_loss / batchcount < minTrainLoss:
+        print('[+] train -- new better loss  %.5f -> %.5f\n' % (minTrainLoss, running_loss / batchcount))
+        old_path = 'weights/train_loss_{}_{}_{:.5f}.pth'.format(args.model, criterion.__class__.__name__, minTrainLoss)
+        if os.path.exists(old_path):
+            os.system('rm ' + old_path)
+        minTrainLoss = running_loss / batchcount
+        savepath = 'weights/train_loss_{}_{}_{:.5f}.pth'.format(args.model, criterion.__class__.__name__, minTrainLoss)
+        torch.save(model.state_dict(), savepath)
+        torch.save(optimizer.state_dict(), savepath.replace("weights", "optimizers"))
     else:
-        print('[-] training -- loss %.5f ' % (running_loss / batchcount))
+        print("[-] train -- loss %.5f\n" % (running_loss / batchcount))
+
+
+    if running_acc / batchcount > maxTrainAcc:
+        print('[+] train -- new better acc  %.5f -> %.5f\n' % (maxTrainAcc, running_acc / batchcount))
+        old_path = 'weights/train_acc_{}_{}_{:.5f}.pth'.format(args.model, "iouscore", maxTrainAcc)
+        if os.path.exists(old_path):
+            os.system('rm ' + old_path)
+        maxTrainAcc = running_acc / batchcount
+        savepath = 'weights/train_acc_{}_{}_{:.5f}.pth'.format(args.model, "iouscore", maxTrainAcc)
+        torch.save(model.state_dict(), savepath)
+    else:
+        print("[-] train -- acc %.5f\n" % (running_acc / batchcount))
 
     with torch.no_grad():
-        validate(model, validloader)
+        validate()
 
 print('Finished Training')
 
